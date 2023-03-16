203a7df16bb81c83578de986353301902b831434
Timan <opiyoadiema@gmail.com>
2023-03-16 16:34:45 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The entry point of the program
+ * Return: 0 (implies success of the code and returns to the main)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
