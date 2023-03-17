59df740eefefc528a542d79ad7832a333c3f36ad
Timan <opiyoadiema@gmail.com>
2023-03-16 19:16:29 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistdio.h>
+/**
+ * main - prints the required statement followed by the line
+ * Return: 1 (success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r-,stdout);
+return (1);
+}
