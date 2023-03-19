b300bb231e085bca76727e3ec8fa8dec475a9526
Timan <opiyoadiema@gmail.com>
2023-03-19 15:38:51 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - refers to the entry point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+
+while (c <= 'z')
+	putchar(c);
+	c++;
+}
+	putchar('\n');
+	Return(0);
