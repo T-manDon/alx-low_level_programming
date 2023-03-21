070e122b12d04f3cd18ed8e92a765c68742c4bfd
Timan <opiyoadiema@gmail.com>
2023-03-20 17:53:23 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-#include <unistdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - prints the required statement followed by the line
- * Return: always 1
+ * Return: 0 (Success)
  */
+
 int main(void)
+
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
