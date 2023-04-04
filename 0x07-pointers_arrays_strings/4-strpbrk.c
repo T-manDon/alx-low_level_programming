d2ea794ebf7d81b566c3b25dd451c235991db093
Timan <opiyoadiema@gmail.com>
2023-04-04 16:08:24 -0700

    4. strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strpbrk - this marks the begining of the code
+ * @s: this refers to the first variable
+ * @k: this refers to the second variable
+ * @accept: this refers to the third variable
+ *
+ * Return: 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+
+{
+	int k;
+
+	while (*s)
+
+	{
+
+	for (k = 0; accept[k]; k++)
+
+	{
+
+	if (*s == accept[k])
+	return (s);
+
+	}
+
+	s++;
+
+	}
+
+	return ('\0');
+}
