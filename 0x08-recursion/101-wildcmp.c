be901fe0fbd7dca5eb292443ec700818a351bf8f
Timan <opiyoadiema@gmail.com>
2023-04-06 18:45:10 -0700

    8. Inception. Now, before you bother telling me it's impossible...

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * wildcmp - used to make comparison
+ * @s1: refers to one of the pointers
+ * @s2: the second pointer
+ *
+ * Return: 0
+ */
+
+int wildcmp(char *s1, char *s2)
+
+{
+
+	if (*s1 == '\0')
+
+	{
+
+	if (*s2 != '\0' && *s2 == '*')
+
+		{
+
+	return (wildcmp(s1, s2 + 1));
+
+		}
+
+	return (*s2 == '\0');
+
+	}
+
+	if (*s2 == '*')
+
+	{
+
+	return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+
+	}
+
+	else if (*s1 == *s2)
+
+	{
+
+	return (wildcmp(s1 + 1, s2 + 1));
+
+	}
+
+	return (0);
+}
