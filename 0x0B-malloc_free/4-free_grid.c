5f0122411a398be2e8061b3ad2870bf0fa5fab73
Timan <opiyoadiema@gmail.com>
2023-04-11 17:29:52 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,27 @@
+#include <stdio.h>		
+#include <stdlib.h>
+#include "main.h"
+
+/**		
+ * free_grid - introduces the code
+ * @grid: first variable
+ * @height: height variable
+ * Description: frees memory of grid
+ * Return: nothing
+ */
+
+void free_grid(int **grid, int height)		
+{
+
+	int i;
+
+	for (i = 0; i < height; i++)
+
+	{
+	free(grid[i]);
+	}
+
+	free(grid);
+		
+}
+
