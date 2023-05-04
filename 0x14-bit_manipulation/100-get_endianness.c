e93948105ff8becf41b005a9e551bd26d170e047
T-manDon <opiyoadiema@gmail.com>
2023-05-04 16:12:46 -0700

    101-password

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - introduces the code
+ * Return: 0 or 1
+ */
+
+int get_endianness(void)
+
+{
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
