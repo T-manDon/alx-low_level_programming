b4ca06d8bca10ce27e2d1576526e5af989dfb62b
T-manDon <opiyoadiema@gmail.com>
2023-08-21 15:39:25 -0700

    operations

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Downloads dynamic library from GitHub to /tmp directory
-wget -P /tmp https://github.com/Lexxyla/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp https://github.com/T-manDon/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
 
-# Set LD_PRELOAD environment variable to preload the downloaded library
 export LD_PRELOAD=/tmp/nrandom.so
