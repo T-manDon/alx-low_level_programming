03899cdaa892009f2aa96961eeefde18ba77c988
T-manDon <opiyoadiema@gmail.com>
2023-08-27 09:23:20 -0700

    winsh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/T-manDon/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
+wget -P /tmp/ https://raw.github.com/Pericles001/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/putshack.so #exporting from the libry
