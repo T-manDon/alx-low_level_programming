55d19f05a843a28b9f8c35fc727f30a3d9e71973
T-manDon <opiyoadiema@gmail.com>
2023-09-14 14:07:28 -0700

    dj

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,21 +1,20 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - Function to compute a hash of 'str' using the djb2 algorithm.
- * @str: The string to hash.
- *
- * Return: The 64-bit hash value.
+ * hash_djb2 - Computes hash
+ * @str: string
+ * Return: value of 64 bits hash
  */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash; // Initialize the hash value.
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381; // Initial hash value defined by the djb2 algorithm.
-    while ((c = *str++)) // Iterate through each character of the input string.
-    {
-        // Update the hash using the djb2 algorithm formula: hash * 33 + c.
-        hash = ((hash << 5) + hash) + c;
-    }
-    return (hash); // Return the calculated hash value.
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
